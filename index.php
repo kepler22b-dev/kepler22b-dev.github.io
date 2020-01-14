2fda22ff8432308233ecde9d2d4636ac7a5fad49
Ryb Al <rybalka_oleksandr@ukr.net>
2020-01-14 22:34:36 +0200

    Init commit2

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
-Hello Country
+<html>
+<body>
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '499806594248695',
+      xfbml      : true,
+      version    : 'v5.0'
+    });
+    FB.AppEvents.logPageView();
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "https://connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
+Hello world
+</body>
+</html>
